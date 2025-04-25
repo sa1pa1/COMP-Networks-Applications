0ae477f01a47bae2d14384c11327bf3ff481289b
sallybui <sallybuipham@gmail.com>
2025-04-25 16:31:51 +0930

    intiailise SR file

---
+++ sr.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "emulator.h"
+#include "sr.h" //changed from gbn.c
+
+/* ******************************************************************
+   Selective Repeat protocol. Adapted from GBN implementation.
+
+  add comments 
+**********************************************************************/
+
