90b8e6892c4adf336d797c257afc27be809f5484
sallybui <sallybuipham@gmail.com>
2025-04-25 16:42:22 +0930

    add ComputeChecksum and IsCorrupted to sr.c

--- sr.c
+++ sr.c
@@ -14,3 +14,32 @@
 #define WINDOWSIZE 6
 #define SEQSPACE 12 //sequence space must be at least 2*windowsize for Selective Repeat -> 
 #define NOTINUSE (-1)
+
+/* generic procedure to compute the checksum of a packet.  Used by both sender and receiver  
+   the simulator will overwrite part of your packet with 'z's.  It will not overwrite your 
+   original checksum.  This procedure must generate a different checksum to the original if
+   the packet is corrupted.
+   
+   NOTE: this function doesn't need to change when transitioning from Go-Back-N to SR since 
+   its utility is only to caclulate checksum, which is the same for both mechanisms. 
+*/
+int ComputeChecksum(struct pkt packet)
+{
+  int checksum = 0;
+  int i;
+
+  checksum = packet.seqnum;
+  checksum += packet.acknum;
+  for ( i=0; i<20; i++ ) 
+    checksum += (int)(packet.payload[i]);
+
+  return checksum;
+}
+
+bool IsCorrupted(struct pkt packet)
+{
+  if (packet.checksum == ComputeChecksum(packet))
+    return (false);
+  else
+    return (true);
+}
