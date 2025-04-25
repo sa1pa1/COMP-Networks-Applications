17b49f5ad9d08b6c04f85aee6d9cf6eded3efde5
sallybui <sallybuipham@gmail.com>
2025-04-25 16:38:03 +0930

    define variables for sr.hc -> sequence space doubled

--- sr.c
+++ sr.c
@@ -10,3 +10,7 @@
   add comments 
 **********************************************************************/
 
+#define RTT 16.0 
+#define WINDOWSIZE 6
+#define SEQSPACE 12 //sequence space must be at least 2*windowsize for Selective Repeat -> 
+#define NOTINUSE (-1)
